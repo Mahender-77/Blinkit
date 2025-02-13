be3d37e98d3c3c137ebb6551c26bb3d6f30ebb9a
Mahendar-77 <mahendarlokini@gmail.com>
2025-02-13 10:58:02 +0530

    search bar added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,12 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import React from "react";
 export const Footer = () => {
+    const [selectedValue, setSelectedValue] = React.useState("Option 1");
+    const [isOpen, setIsOpen] = React.useState(false);
+  
+    const options = ["Option 1", "Option 2", "Option 3"];
   return (
     <Box>
       <Box
@@ -118,6 +123,40 @@ export const Footer = () => {
           <LinkedInIcon sx={{ fontSize: "40px" ,color:"#0e76a8" }} />
         </Box>
       </Box>
+      <div className="relative w-64 mx-auto mt-10">
+      {/* Input Box (Acts like a dropdown trigger) */}
+      <input
+        type="text"
+        value={selectedValue}
+        readOnly
+        className="border p-2 w-full rounded cursor-pointer bg-white"
+        onClick={() => setIsOpen(!isOpen)}
+      />
+
+      {/* Dropdown Menu */}
+      {isOpen && (
+        <div className="absolute w-full mt-1 border rounded bg-white shadow">
+          {options.map((option) => (
+            <label
+              key={option}
+              className="flex items-center gap-2 p-2 hover:bg-gray-100 cursor-pointer"
+            >
+              <input
+                type="radio"
+                name="options"
+                value={option}
+                checked={selectedValue === option}
+                onChange={() => {
+                  setSelectedValue(option);
+                  setIsOpen(false);
+                }}
+              />
+              {option}
+            </label>
+          ))}
+        </div>
+      )}
+    </div>
     </Box>
   );
 };
